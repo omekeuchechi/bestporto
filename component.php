b3d4c9d0aa4453565ba95aa3cb6779a02883dac9
omekeuchechi <omekejoseph97@gmail.com>
2024-07-10 13:35:58 +0100

    what a design

---
+++ component.php
@@ -0,0 +1,3 @@
+<?php
+echo `<p style="color: red;">hollo world</p>`
+?>
---
+++ simple.js
@@ -0,0 +1,6 @@
+var typed6 = new Typed('#typed6', {
+    strings: ['npm install^1000\n `installing components...` ^1000\n `Fetching from source...`'],
+    typeSpeed: 40,
+    backSpeed: 0,
+    loop: true
+  });
